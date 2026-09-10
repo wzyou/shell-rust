2b570cd308c06a5d4dfd5ab7243e42475196770d
wzyou <wzyou.goo@gmail.com>
2026-09-10 21:08:58 +0800

    codecrafters submit [skip ci]

--- your_program.sh
+++ your_program.sh
@@ -1,24 +1,11 @@
 #!/bin/sh
 #
-# Use this script to run your program LOCALLY.
+# This script is used to run your program on CodeCrafters
 #
-# Note: Changing this script WILL NOT affect how CodeCrafters runs your program.
+# This runs after .codecrafters/compile.sh
 #
 # Learn more: https://codecrafters.io/program-interface
 
-set -e # Exit early if any commands fail
+set -e # Exit on failure
 
-# Copied from .codecrafters/compile.sh
-#
-# - Edit this to change how your program compiles locally
-# - Edit .codecrafters/compile.sh to change how your program compiles remotely
-(
-  cd "$(dirname "$0")" # Ensure compile steps are run within the repository directory
-  cargo build --release --target-dir=/tmp/codecrafters-build-shell-rust --manifest-path Cargo.toml
-)
-
-# Copied from .codecrafters/run.sh
-#
-# - Edit this to change how your program runs locally
-# - Edit .codecrafters/run.sh to change how your program runs remotely
 exec /tmp/codecrafters-build-shell-rust/release/codecrafters-shell "$@"
