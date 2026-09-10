155e78ac26552a92a85e752030b9dd7250b14587
wzyou <wzyou.goo@gmail.com>
2026-09-10 19:22:42 +0800

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -5,4 +5,7 @@ fn main() {
     // TODO: Uncomment the code below to pass the first stage
     print!("$ ");
     io::stdout().flush().unwrap();
+    let mut command = String::new();
+    io::stdin().read_line(&mut command).unwrap();
+    println!("{}: command not found!", command.trim());
 }
