7ee1e07549591643b71331507c03ded6d971e26e
wzyou <wzyou.goo@gmail.com>
2026-09-10 19:23:20 +0800

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -7,5 +7,5 @@ fn main() {
     io::stdout().flush().unwrap();
     let mut command = String::new();
     io::stdin().read_line(&mut command).unwrap();
-    println!("{}: command not found!", command.trim());
+    println!("{}: command not found", command.trim());
 }
