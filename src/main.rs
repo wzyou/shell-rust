06593504596ce7822edfbb38b5487b0fb604e624
wzyou <wzyou.goo@gmail.com>
2026-09-10 20:01:54 +0800

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -3,33 +3,21 @@ use std::io::{self, Write};
 
 fn main() {
     // TODO: Uncomment the code below to pass the first stage
-    let builtin_commands = [
-        "exit",
-        "echo",
-        "type",
-    ];
     loop {
         print!("$ ");
         io::stdout().flush().unwrap();
         let mut command = String::new();
         io::stdin().read_line(&mut command).unwrap();
-        let command = command.trim().to_string();
-        if command == "exit" {
-            break;
+        
+        let full_command:Vec<&str> = command.split_whitespace().collect();
+
+        match full_command.as_slice() {
+            [] => continue,
+            ["exit"] => break,
+            ["echo", rest @ ..] => println!("{}", rest.join("")),
+            ["type", rest @ ("exit" | "echo" | "type")] => println!("{} is a shell builtin", rest),
+            ["type", rest @ ..] =>  println!("{}: not found", rest[0]),
+            _ => println!("{}: command not found", full_command[0]),
         }
-        if command.starts_with("echo ") {
-            println!("{}", &command[5..]);
-            continue;
-        }
-        if command.starts_with("type ") {
-            let o_command = command[5..].to_string();
-            if builtin_commands.iter().any(|&c| *c == *o_command ) {
-                println!("{} is a shell builtin", o_command);
-            } else {
-                println!("{}: not found", o_command);
-            }
-            continue;
-        }
-        println!("{}: command not found", command);
     }
 }
