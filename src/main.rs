751e04f15cd248387263e1ac8484a8169679016f
wzyou <wzyou.goo@gmail.com>
2026-09-10 19:49:05 +0800

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,11 @@ use std::io::{self, Write};
 
 fn main() {
     // TODO: Uncomment the code below to pass the first stage
+    let builtin_commands = [
+        "exit",
+        "echo",
+        "type",
+    ];
     loop {
         print!("$ ");
         io::stdout().flush().unwrap();
@@ -14,6 +19,15 @@ fn main() {
         }
         if command.starts_with("echo ") {
             println!("{}", &command[5..]);
+            continue;
+        }
+        if command.starts_with("type ") {
+            let o_command = command[5..].to_string();
+            if builtin_commands.iter().any(|&c| *c == *o_command ) {
+                println!("{} is a shell builtin", o_command);
+            } else {
+                println!("{}: command not found", o_command);
+            }
             continue;
         }
         println!("{}: command not found", command);
