ff72457dca40430a6ad1a13fb47311ef605c1727
wzyou <wzyou.goo@gmail.com>
2026-09-10 19:32:13 +0800

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -8,6 +8,10 @@ fn main() {
         io::stdout().flush().unwrap();
         let mut command = String::new();
         io::stdin().read_line(&mut command).unwrap();
-        println!("{}: command not found", command.trim());
+        let command = command.trim().to_string();
+        if command == "exit" {
+            break;
+        }
+        println!("{}: command not found", command);
     }
 }
